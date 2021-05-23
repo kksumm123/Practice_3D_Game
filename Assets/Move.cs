6e8b5f5da56f725bae05b2ff2b54729fa9e4a13d
kksumm123 <kksumm123@gmail.com>
2021-05-23 23:53:55 +0900

    Initial commit

---
+++ Assets/Move.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed = 3f;
+
+    Rigidbody rgdbody;
+    private void Start()
+    {
+        rgdbody = GetComponent<Rigidbody>();
+    }
+
+    Vector3 pos;
+    float moveX;
+    float moveZ;
+    public float forceY = 300;
+    
+    void Update()
+    {
+        Move4Dir();
+
+        if (transform.position.y == 0 && Input.GetKeyDown(KeyCode.Space))
+            rgdbody.AddForce(0, forceY, 0);
+    }
+
+    private void Move4Dir()
+    {
+        moveX = 0; moveZ = 0;
+        if (Input.GetKey(KeyCode.W))
+            moveZ = 1;
+        if (Input.GetKey(KeyCode.S))
+            moveZ = -1;
+        if (Input.GetKey(KeyCode.A))
+            moveX = -1;
+        if (Input.GetKey(KeyCode.D))
+            moveX = 1;
+
+        pos = transform.position;
+        pos.x += moveX * speed * Time.deltaTime;
+        pos.z += moveZ * speed * Time.deltaTime;
+        transform.position = pos;
+    }
+}
