67ef7c9c99af3aa75ec7c4a5836fffdede21fb47
kksumm123 <kksumm123@gmail.com>
2021-05-24 00:09:34 +0900

    editorconfig 추가, 마우스 숨기기, 잠금 추가

---
+++ Assets/GameManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.visible = false;                     //마우스 커서가 보이지 않게 함
+        Cursor.lockState = CursorLockMode.Locked;   //마우스 커서를 고정시킴
+    }
+
+    void Update()
+    {
+        
+    }
+}
